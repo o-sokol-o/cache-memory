1b39c57494442195787c7fbc13f9acde44f30bf2
o-sokol-o <sokol.pad@gmail.com>
2022-07-18 22:35:52 +0300

    Release 0.0.1

--- cache-memory.go
+++ cache-memory.go
@@ -1,29 +1,73 @@
 package cachememory
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/zhashkevych/scheduler"
+)
+
+var ()
+
 type CacheMemory interface {
 	Set(key string, value interface{})
-	Get(key string) interface{}
+	Get(key string) (interface{}, error)
 	Delete(key string)
 }
 
-type Cache struct {
-	m map[string]interface{}
+type cacheValue struct {
+	t    time.Time
+	data interface{}
 }
 
-func New() CacheMemory {
-	var c Cache
-	c.m = make(map[string]interface{})
+type Cache struct {
+	worker *scheduler.Scheduler
+	ctx    context.Context
+	lt     int64 // default life time
+	cv     map[string]cacheValue
+}
+
+func New(lifeTimeSec int64) *Cache {
+	c := Cache{
+		ctx:    context.Background(),
+		worker: scheduler.NewScheduler(),
+		lt:     lifeTimeSec * 1000000000, // default life time nanosecond
+		cv:     make(map[string]cacheValue)}
+
+	c.worker.Add(c.ctx,
+		func(ctx context.Context) {
+			func() {
+				for k, v := range c.cv {
+					if time.Now().UnixNano()-v.t.UnixNano() >= c.lt {
+						fmt.Println(k + " - key deleted")
+						delete(c.cv, k)
+					}
+				}
+			}()
+		},
+		time.Second)
+
 	return &c
 }
 
 func (c Cache) Set(key string, value interface{}) {
-	c.m[key] = value
+	c.cv[key] = cacheValue{time.Now(), value}
 }
 
-func (c Cache) Get(key string) interface{} {
-	return c.m[key]
+func (c Cache) Get(key string) (interface{}, error) {
+	if v, ok := c.cv[key]; ok {
+		return v.data, nil
+	}
+	return nil, errors.New(key + " - key absent")
 }
 
 func (c Cache) Delete(key string) {
-	delete(c.m, key)
+	fmt.Println(key + " - key deleted")
+	delete(c.cv, key)
+}
+
+func (c Cache) Free() {
+	c.worker.Stop()
 }
--- go.mod
+++ go.mod
@@ -1,2 +1,5 @@
 module github.com/o-sokol-o/cache-memory
 
+go 1.18
+
+require github.com/zhashkevych/scheduler v1.0.0 // indirect
